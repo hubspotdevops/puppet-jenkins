bc2d1b3127be3c90d316b2260542e5df9d234dad
Tom McLaughlin <tmclaughlin@hubspot.com>
2013-08-20 10:15:43 -0400

    Expose $plugin_dir.  Necessary if JENKINS_HOME is set in sysconfig.

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -1,9 +1,10 @@
 #
 #
 #
-define jenkins::plugin($version=0) {
-  $plugin            = "${name}.hpi"
-  $plugin_dir        = '/var/lib/jenkins/plugins'
+define jenkins::plugin($version=0,
+                       $plugin_dir='/var/lib/jenkins/plugins'
+) {
+  $plugin = "${name}.hpi"
 
   if ($version != 0) {
     $base_url = "http://updates.jenkins-ci.org/download/plugins/${name}/${version}/"
