edffbd88dd9d366c402f61fb7e75812fd349cbc0
Tom McLaughlin <tmclaughlin@hubspot.com>
2013-08-20 10:03:11 -0400

    Remove $plugin_parent_dir since it's no longer necessary.

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -4,7 +4,6 @@
 define jenkins::plugin($version=0) {
   $plugin            = "${name}.hpi"
   $plugin_dir        = '/var/lib/jenkins/plugins'
-  $plugin_parent_dir = '/var/lib/jenkins'
 
   if ($version != 0) {
     $base_url = "http://updates.jenkins-ci.org/download/plugins/${name}/${version}/"
